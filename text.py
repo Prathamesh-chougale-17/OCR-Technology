df5880209b3a14f029e4d22ef7c2097471e3fc4d
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-09-28 15:36:49 +0530

    saving the successfully working of tesserant

---
+++ text.py
@@ -0,0 +1,15 @@
+import pytesseract as pt
+
+pt.pytesseract.tesseract_cmd = r"D:\Tesserract\tesseract.exe"
+from PIL import Image
+
+# import cv2
+
+img = Image.open("text.jpg")
+text = pt.image_to_string(img)
+
+# pt.pytesseract.tesseract_cmd = "D:\\Tesserract\\tesseract.exe"
+
+# text = pt.image_to_string(img)
+
+print(text)
